3dde6695735f7f39c73e7f940d25f53c9079f8aa
변찬우 <byun00106@gmail.com>
2024-05-16 20:31:59 +0900

    feat: add app layout

---
+++ note-mark/src/renderer/src/components/AppLayout.tsx
@@ -0,0 +1,26 @@
+import { cn } from '@renderer/utils'
+import { ComponentProps, forwardRef } from 'react'
+
+export const RootLayout = ({ children, className, ...props }: ComponentProps<'main'>) => {
+  return (
+    <main className={cn('flex flex-row h-screen', className)} {...props}>
+      {children}
+    </main>
+  )
+}
+
+export const Sidebar = ({ className, children, ...props }: ComponentProps<'aside'>) => {
+  return (
+    <aside className={cn('w-[250px] mt-10 h-[100vh + 10px] overflow-auto', className)} {...props}>
+      {children}
+    </aside>
+  )
+}
+
+export const Content = forwardRef<HTMLDivElement, ComponentProps<'div'>>(
+  ({ children, className, ...props }, ref) => {
+    return <div ref={ref} className={cn('flex-1 h-full overflow-auto', className)} {...props}></div>
+  }
+)
+
+Content.displayName = 'Content'
