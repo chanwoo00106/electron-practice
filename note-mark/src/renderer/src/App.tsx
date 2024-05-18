d5898e3a7357d96bc8bd74440c736b48a17218e7
변찬우 <byun00106@gmail.com>
2024-05-18 23:45:47 +0900

    feat: add markdown editor

--- note-mark/src/renderer/src/App.tsx
+++ note-mark/src/renderer/src/App.tsx
@@ -6,6 +6,7 @@ import {
   RootLayout,
   Sidebar
 } from './components'
+import { MarkdownEditor } from './components/MarkdownEditor'
 
 const App = (): JSX.Element => {
   return (
@@ -16,7 +17,9 @@ const App = (): JSX.Element => {
           <ActionButtonRow className="flex justify-between mt-1" />
           <NotePreviewList className="mt-3 space-y-1" />
         </Sidebar>
-        <Content className="border-l bg-zinc-900/50 border-l-white/20">Content</Content>
+        <Content className="border-l bg-zinc-900/50 border-l-white/20">
+          <MarkdownEditor />
+        </Content>
       </RootLayout>
     </>
   )
